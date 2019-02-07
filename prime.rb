90ccacf5e5a21c615c7f0292bad0f61b488369cc
Johnson Joseph <jhonsonj88@gmail.com>
2019-02-07 18:06:42 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(num)
+ if num == 2 || num % 3 
+   true 
+ else 
+   num <= -1
+   false 
+ end 
+end # Add  code here!
+
+#greater than 1 is a prime number, 2 is the only even prime number, any odd numbers are prime numbers 
