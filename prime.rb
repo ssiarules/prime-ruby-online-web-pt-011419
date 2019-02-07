ae7de8acad9fe621f64cab030e9339ac1fc06e75
Johnson Joseph <jhonsonj88@gmail.com>
2019-02-07 18:39:03 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,20 @@
 
 
 def prime?(num)
- if num == 2 || num % 3 
-   true 
- elsif -1 == 2 || -1 % 3 
- #elsif num <= -1
-   false 
- end 
+  if num <= 1 
+    false 
 end 
+end 
+
+
+
+
+# if num == 2 || num % 3 
+#   true 
+# elsif -1 == 2 || -1 % 3 
+# #elsif num <= -1
+#   false 
+# end 
+# end 
 
 #greater than 1 is a prime number, 2 is the only even prime number, any odd numbers are prime numbers 
